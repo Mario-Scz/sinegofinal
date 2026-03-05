8f86079745198a7be29ab0a70f48fc96a1998d4c
Mario Sánchez <mroberto.drako@gmail.com>
2026-03-04 20:31:11 -0600

    Desactivando htaccess temporalmente

--- config/db.php
+++ config/db.php
@@ -20,9 +20,10 @@ $host = getenv('DB_HOST') ?: 'localhost';
 $db   = getenv('DB_NAME') ?: 'sinego';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
+$port = getenv('DB_PORT') ?: 3306;
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
